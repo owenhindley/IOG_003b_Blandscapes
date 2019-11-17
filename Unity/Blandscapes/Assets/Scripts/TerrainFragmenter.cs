b9b9b96358f06da55fdea4c5e7de00f96a5b20b9
owenhindley <owenhindley@hotmail.com>
2019-11-17 03:40:25 +0000

    working!

--- Unity/Blandscapes/Assets/Scripts/TerrainFragmenter.cs
+++ Unity/Blandscapes/Assets/Scripts/TerrainFragmenter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TerrainFragmenter : MonoBehaviour
 {
     public GameObject sourceObject;
+    public GameObject thufaPrefab;
     public Mesh sourceMesh; 
 
     public Material triangleMaterial;
@@ -13,6 +14,7 @@ public class TerrainFragmenter : MonoBehaviour
     public int numTrianglesToExtract = 64;
 
     public float normalThreshold = 0.8f;
+    public float separationDistance = 0.01f;
 
     [InspectorButton("GrabTriangles")] public bool DoGrabTriangles;
     [InspectorButton("DestroyTriangles")] public bool DoDestroyTriangles;
@@ -36,7 +38,7 @@ public class TerrainFragmenter : MonoBehaviour
 
         while(extractedTriangles.Count < numTrianglesToExtract) {
 
-            int index = Random.Range(0, numTriangles);
+            int index = Random.Range(0, numTriangles-1);
             Debug.Log("index : " + index);
             var vert0 = sourceMesh.triangles[index * 3];
             var vert1 = sourceMesh.triangles[(index*3)+1];
@@ -46,11 +48,15 @@ public class TerrainFragmenter : MonoBehaviour
             var pos1 = sourceMesh.vertices[vert1];
             var pos2 = sourceMesh.vertices[vert2];
 
-            var uv0_0 = sourceMesh.uv[index];
-            var uv0_1 = sourceMesh.uv[index+2];
-            var uv0_2 = sourceMesh.uv[index+3];
+            var uv0_0 = sourceMesh.uv[vert0];
+            var uv0_1 = sourceMesh.uv[vert1];
+            var uv0_2 = sourceMesh.uv[vert2];
 
-            var triNormal = sourceMesh.normals[index];
+            var triNormal = sourceMesh.normals[vert0];
+
+            pos0 += triNormal * separationDistance;
+            pos1 += triNormal * separationDistance;
+            pos2 += triNormal * separationDistance;
 
             if (Vector3.Dot(triNormal, Vector3.up) >= normalThreshold){
 
@@ -58,22 +64,35 @@ public class TerrainFragmenter : MonoBehaviour
                 var mid12 = Vector3.Lerp(pos1, pos2, 0.5f);
                 var mid21 = Vector3.Lerp(pos2, pos1, 0.5f);
 
+                var norm0 = sourceMesh.normals[vert0];
+                var norm1 = sourceMesh.normals[vert0];
+                var norm2 = sourceMesh.normals[vert0];
+
 
                 var pivotPoint = mid01;
-                if (mid12.x < pivotPoint.x) pivotPoint = mid12;
-                if (mid21.x < pivotPoint.x) pivotPoint = mid21;
+                // if (mid12.x < pivotPoint.x) pivotPoint = mid12;
+                // if (mid21.x < pivotPoint.x) pivotPoint = mid21;
 
                 pos0 -= pivotPoint;
                 pos1 -= pivotPoint;
                 pos2 -= pivotPoint;
+
+                var rotNormalise = Quaternion.FromToRotation(pos1 - pos0, Vector3.left);
+                pos0 = rotNormalise * pos0;
+                pos1 = rotNormalise * pos1;
+                pos2 = rotNormalise * pos2;
+
+                
+            
                 
                 var newMesh = new Mesh();
                 newMesh.vertices = new Vector3[] { pos0, pos1, pos2};
                 newMesh.uv = new Vector2[] { uv0_0, uv0_1, uv0_2};
-                newMesh.triangles = new int[] { 0, 1, 2, 0, 2, 1};
-                
+                newMesh.normals = new Vector3[] { rotNormalise * norm0, rotNormalise * norm1, rotNormalise * norm2 };
+                newMesh.triangles = new int[] { 0, 1, 2, 0, 2, 1 };
 
-                var go = new GameObject();
+
+                var go = GameObject.Instantiate(thufaPrefab, Vector3.zero, Quaternion.identity);
                 var mf = go.AddComponent<MeshFilter>();
                 mf.mesh = newMesh;
                 var newMr = go.AddComponent<MeshRenderer>();
@@ -81,6 +100,7 @@ public class TerrainFragmenter : MonoBehaviour
                 
                 go.transform.parent = transform;
                 go.transform.localPosition = pivotPoint;
+                go.transform.localRotation = Quaternion.Inverse(rotNormalise);
                 go.transform.localScale = Vector3.one;
                 go.name = "TrianglePiece" + extractedTriangles.Count;
 
---
+++ Unity/Blandscapes/Assets/Scripts/ThufaTriangle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ThufaTriangle : MonoBehaviour
+{
+    public Collider triggerCollider;
+    public AnimationCurve flutterAnimation;
+
+    // public Light pointLight;
+
+    public float openAngle = 30.0f;
+    public float openTime = 0.5f;
+
+    public bool isOpen = false;
+
+    private float normalLightIntens = 0.0f;
+
+    [InspectorButton("DoFlutter")] public bool DoFlutterButton;
+
+    private Quaternion originalRotation;
+    private Quaternion openRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalRotation = transform.localRotation;
+        openRotation = originalRotation * Quaternion.Euler(-openAngle, 0.0f, 0.0f);
+    //     if (pointLight == null) { pointLight = GetComponentInChildren<Light>(); }
+    //     normalLightIntens= pointLight.intensity;
+    //     pointLight.intensity = 0.0f;
+    //     pointLight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var isOpenNow = triggerCollider.bounds.Contains(transform.position);
+        if (isOpenNow != isOpen){
+            if (isOpenNow) { DoFlutter(); }
+            isOpen = isOpenNow;
+        }
+    }
+
+    public void DoFlutter(){
+        // pointLight.enabled = true;
+        // pointLight.DOIntensity(normalLightIntens, 1.0f);
+        transform.DOLocalRotate(openRotation.eulerAngles, openTime, RotateMode.Fast).OnComplete(()=>{
+            if (isOpen){
+                transform.DOLocalRotate(originalRotation.eulerAngles, openTime * 2.0f).SetEase(Ease.InOutBounce).OnComplete(()=>{
+                    if (isOpen){
+                        DoFlutter();
+                    }
+                });
+            } else {
+                transform.DOLocalRotate(originalRotation.eulerAngles, openTime/4.0f).SetEase(Ease.InOutCubic);
+                //  pointLight.DOIntensity(normalLightIntens, 1.0f).OnComplete(()=>{
+                //       pointLight.enabled = false;
+                //  });
+            }
+        }).SetEase(Ease.InOutCubic).SetDelay(Random.Range(0.0f, openTime));
+
+    }
+
+}
